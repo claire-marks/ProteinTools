f5d62575f4defaab3d47bb5b61b5b1ad73a2369a
Claire Marks <clairemarks89@gmail.com>
2020-05-14 21:21:50 +0100

    Added setup script

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Extension
+import numpy as np
+
+numpy_include_dir = np.get_include()
+
+# Setup script written by Jinwoo Leem
+setup(name = "protmod",
+      version = "1.0",
+      description = "Useful things for dealing with proteins!",
+      author = "Claire Marks",
+      author_email = "marks@stats.ox.ac.uk",
+      packages = [
+          "protmod",
+      ],
+      package_dir = {
+          "protmod": "lib/python/protmod",
+      },
+      scripts = ["bin/LoopFinder"]
+)
